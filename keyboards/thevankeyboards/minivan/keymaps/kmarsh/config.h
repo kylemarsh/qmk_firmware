2117bbd6a74218110ec17cca053f557ce21cabc1
Kyle Marsh <kmarsh@etsy.com>
2019-07-26 20:59:11 -0700

    tweak the firmware a little

--- keyboards/thevankeyboards/minivan/keymaps/kmarsh/config.h
+++ keyboards/thevankeyboards/minivan/keymaps/kmarsh/config.h
@@ -39,7 +39,7 @@
 //#define USB_MAX_POWER_CONSUMPTION 100
 #define TAPPING_TERM      175
 #define PERMISSIVE_HOLD
-//#define IGNORE_MOD_TAP_INTERRUPT
+#define IGNORE_MOD_TAP_INTERRUPT
 
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
--- keyboards/thevankeyboards/minivan/keymaps/kmarsh/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/kmarsh/keymap.c
@@ -38,18 +38,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QW] = LAYOUT_arrow( // QWERTY base layer
         KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
         LT(2, KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, LT(2, KC_QUOT),
-        LSFT_T(KC_LBRC), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_RBRC),
+        KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL,         KC_LALT, KC_LGUI,          LT(1, KC_BSPC), LT(1, KC_SPC),      KC_RGUI, KC_RALT, KC_RCTL, TG(3)
     ),
     [_SY] = LAYOUT_arrow( // symbol layer
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_COLN,
-        KC_TRNS, KC_BTN3, KC_BTN2, KC_MS_U, KC_BTN1, KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_EQL, KC_TRNS,
-        KC_TRNS, KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_UNDS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,                   LT(1, KC_DEL), KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN, KC_COLN,
+        KC_TRNS, KC_BTN3, KC_UNDS, KC_LCBR, KC_RCBR, KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_EQL,  KC_TRNS,
+        KC_TRNS, KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_UNDS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,                   LT(1, KC_DEL), KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS
     ),
     [_NP] = LAYOUT_arrow( // numpad layer
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
-        KC_TRNS, KC_NO,   KC_MINS, KC_LCBR, KC_RCBR, KC_NO,   KC_MINS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+        KC_TRNS, KC_NO,   KC_MINS, KC_LCBR, KC_RCBR, KC_NO,   KC_MINS, KC_4,    KC_5,    KC_6,    KC_PLUS,  KC_TRNS,
         KC_TRNS, KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_NO,   KC_0,    KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                   LT(1, KC_BSPC), KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
